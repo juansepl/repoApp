3f49d9a1d0c603542657145d68341f50477f50de
Juanse Perez <jsperez@buk.cl>
2024-01-17 16:53:23 -0500

    feat: create records fake

--- Gemfile
+++ Gemfile
@@ -62,6 +62,9 @@ group :development do
 
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
+
+  # create objects dummy
+  gem 'faker'
 end
 
 group :test do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Create dummy records
+100.times do
+  Repository.create!(
+    id_repo: Faker::Number.unique.number(digits: 7),
+    login: Faker::Internet.unique.username(specifier: 10),
+    avatar: Faker::Avatar.image(size: '50x50'),
+    url: Faker::Internet.unique.url(host: 'github')
+  )
+end
+
+puts ':::.. Repositories created ..:::'
