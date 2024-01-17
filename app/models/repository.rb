1eb62af362c5b75b153c6ab2484e148be039d850
Juanse Perez <jsperez@buk.cl>
2024-01-17 17:13:50 -0500

    feat: validations for manage repository

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,3 +1,12 @@
+#
+# Model Repository
+#
 class Repository < ApplicationRecord
   paginates_per 10
+
+  # Validations
+
+  validates :login, presence: true
+  # Control for fields with format URL
+  validates :avatar, :url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp }
 end
